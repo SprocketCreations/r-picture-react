1121167a7ff219adc5380dc125632768388c01d4
Kailen James <sevenflags57@gmail.com>
2023-03-13 16:27:36 -0700

    Comment section

--- src/components/comments/index.js
+++ src/components/comments/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 export default function Comments(props) {
+	const [text, setText] = useState("");
+
+	const submit = event => {
+		event.preventDefault();
+
+		if (text) {
+
+			setText("");
+		}
+	}
+
 	return (
 		<section className="comments">
 			<header>
-				<form>
-					<textarea name="comment-text" id="comment-text" cols="30" rows="10"></textarea>
+				<form onSubmit={submit}>
+					<textarea
+						value={text}
+						onChange={event => setText(event.target.value)}
+						name="comment-text"
+						id="comment-text"
+						cols="30"
+						rows="10"></textarea>
 					<button>Submit</button>
 				</form>
 			</header>
