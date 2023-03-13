ca55bd61c8ffcea7a46e75f70715c9714a090eff
Kailen James <sevenflags57@gmail.com>
2023-03-13 10:38:54 -0700

    Navigator and twocolumn framing

--- src/components/navigator/index.js
+++ src/components/navigator/index.js
@@ -5,16 +5,18 @@ import { Link } from "react-router-dom";
 export default function Navigator() {
 	return (
 		<header className="navigator">
-			<Link to="/">Home</Link>
-			<input type="search" name="search" id="search" />
-			<p>
-				Use a # to search for a tag
-			</p>
-			<Link to="/user/1">Profile</Link>
-			<Link to="/picture/new">New Picture</Link>
-			<Link to="/gallery/new">New Gallery</Link>
-			<Link to="/signin">Sign In</Link>
-			<Link to="javascript:void(0);">Sign Out</Link>
+			<nav>
+				<Link to="/">Home</Link>
+				<input type="search" name="search" id="search" />
+				<p>
+					Use a # to search for a tag
+				</p>
+				<Link to="/user/1">Profile</Link>
+				<Link to="/picture/new">New Picture</Link>
+				<Link to="/gallery/new">New Gallery</Link>
+				<Link to="/signin">Sign In</Link>
+				<Link to="javascript:void(0);">Sign Out</Link>
+			</nav>
 		</header>
 	);
 }
