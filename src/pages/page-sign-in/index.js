1ab4912b8eef6db66fc76f9662a80a00f6bdd143
Kailen James <sevenflags57@gmail.com>
2023-03-13 16:16:49 -0700

    sign in page form

--- src/pages/page-sign-in/index.js
+++ src/pages/page-sign-in/index.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 import { Link } from "react-router-dom";
 import { TwoColumn } from "../../components";
 
 export default function PageSignIn() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const validateEmail = email => /^[\w\.-]+@([\w-]+\.)+[\w]{2,8}$/.test(email);
+	const validatePassword = password => !/^(.{0,7}|[^A-Z]*|[^a-z]*|[^0-9]*|[A-Za-z0-9]*)$/.test(password);
+	
+	const submit = event => {
+		event.preventDefault();
+
+		if(validateEmail(email) && validatePassword(password)) {
+			setEmail("");
+			setPassword("");
+		}
+	};
+
 	return (
 		<main className="page-sign-in">
 			<TwoColumn left={
@@ -13,11 +28,31 @@ export default function PageSignIn() {
 						<h2>Sign In</h2>
 					</header>
 					<main>
-						<form id="sign-in-form" className="sign-in-form">
-							<label htmlFor="sign-in-email">Email</label>
-							<input type="sign-in-email" id="sign-in-email" />
-							<label htmlFor="sign-in-password">Password</label>
-							<input type="sign-in-password" id="sign-in-password" />
+						<form onSubmit={submit} id="sign-in-form" className="sign-in-form">
+							<label htmlFor="sign-in-email">
+								<span>Email</span>
+								{!email && <span>Required</span>}
+								{email && (validateEmail(email) || <span>Invalid</span>)}
+							</label>
+							<input
+								value={email}
+								onChange={event => setEmail(event.target.value.trim())}
+								type="text"
+								name="sign-in-email"
+								id="sign-in-email" />
+
+							<label htmlFor="sign-in-password">
+								<span>Password</span>
+								{!password && <span>Required</span>}
+								{password && (validatePassword(password) || <span>Invalid</span>)}
+							</label>
+							<input
+								value={password}
+								onChange={event => setPassword(event.target.value)}
+								type="password"
+								name="sign-in-password"
+								id="sign-in-password" />
+
 							<button>Sign In</button>
 						</form>
 					</main>
