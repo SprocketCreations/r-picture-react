c2e837467a29eab5ed76949a562cf4b95c688e11
Kailen James <sevenflags57@gmail.com>
2023-03-13 11:02:43 -0700

    signin/signup

--- src/pages/page-sign-up/index.js
+++ src/pages/page-sign-up/index.js
@@ -1,22 +1,28 @@
 import React from "react";
 import "./style.css";
 
+import { TwoColumn } from "../../components";
+
 export default function PageSignUp() {
 	return (
 		<main className="page-sign-up">
-			<form id="sign-up-form">
-				<label htmlFor="sign-up-display-name">Name</label>
-				<input type="text" id="sign-up-display-name" />
+			<TwoColumn left={
+				<section className="sign-up-wrapper">
+					<form id="sign-up-form" className="sign-up-form">
+						<label htmlFor="sign-up-display-name">Name</label>
+						<input type="text" id="sign-up-display-name" />
 
-				<label htmlFor="sign-up-email">Email</label>
-				<input type="email" id="sign-up-email" />
+						<label htmlFor="sign-up-email">Email</label>
+						<input type="email" id="sign-up-email" />
 
-				<label htmlFor="sign-up-password-1">Password</label>
-				<input type="password" id="sign-up-password-1" />
-				<label htmlFor="sign-up-password-2">Repeat Password</label>
-				<input type="password" id="sign-up-password-2" />
-				<button >Sign Up</button>
-			</form>
+						<label htmlFor="sign-up-password-1">Password</label>
+						<input type="password" id="sign-up-password-1" />
+						<label htmlFor="sign-up-password-2">Repeat Password</label>
+						<input type="password" id="sign-up-password-2" />
+						<button >Sign Up</button>
+					</form>
+				</section>
+			} />
 		</main>
 	);
 }
