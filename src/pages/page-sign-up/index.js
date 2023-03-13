aabda4ab7bd717fb3c034cffd7b8d3a23128d229
Kailen James <sevenflags57@gmail.com>
2023-03-13 14:31:32 -0700

    Styled sign in and sign up pages

--- src/pages/page-sign-up/index.js
+++ src/pages/page-sign-up/index.js
@@ -8,19 +8,24 @@ export default function PageSignUp() {
 		<main className="page-sign-up">
 			<TwoColumn left={
 				<section className="sign-up-wrapper">
-					<form id="sign-up-form" className="sign-up-form">
-						<label htmlFor="sign-up-display-name">Name</label>
-						<input type="text" id="sign-up-display-name" />
+					<header>
+						<h2>Sign Up</h2>
+					</header>
+					<main>
+						<form id="sign-up-form" className="sign-up-form">
+							<label htmlFor="sign-up-display-name">Name</label>
+							<input type="text" id="sign-up-display-name" />
 
-						<label htmlFor="sign-up-email">Email</label>
-						<input type="email" id="sign-up-email" />
+							<label htmlFor="sign-up-email">Email</label>
+							<input type="email" id="sign-up-email" />
 
-						<label htmlFor="sign-up-password-1">Password</label>
-						<input type="password" id="sign-up-password-1" />
-						<label htmlFor="sign-up-password-2">Repeat Password</label>
-						<input type="password" id="sign-up-password-2" />
-						<button >Sign Up</button>
-					</form>
+							<label htmlFor="sign-up-password-1">Password</label>
+							<input type="password" id="sign-up-password-1" />
+							<label htmlFor="sign-up-password-2">Repeat Password</label>
+							<input type="password" id="sign-up-password-2" />
+							<button >Sign Up</button>
+						</form>
+					</main>
 				</section>
 			} />
 		</main>
